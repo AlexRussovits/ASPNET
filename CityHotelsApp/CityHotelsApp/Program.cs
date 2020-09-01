970cc642deaa677028495e5f32e4889e9c0a60a4
pupil <pupil@ivkhk.ee>
2020-09-01 15:02:42 +0300

    Массивы и Практика в C#

---
+++ CityHotelsApp/CityHotelsApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace CityHotelsApp
+{
+    class Program
+    {
+        //Aleksander Russovits JPTVR18
+
+        class Hotel
+        {
+            public string City { get; set; }
+            public string HotelName { get; set; }
+            public string Address { get; set; }
+            public string Services { get; set; }
+
+            public override string ToString()
+            {
+                return "Hotel: " + HotelName + " " + City + " " + Address + " " + Services; 
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Hotel[] hotels =
+            {
+                new Hotel{HotelName = "Radisson Sky Blue", City = "Tallinn", Address = "Rävala pst 3", Services = "Wi-fi, Parking, SPA"},
+                new Hotel{HotelName = "Hilton Tallinn Park", City = "Tallinn", Address = "Fr. R. Kreutzwaldi 23", Services = "Wi-fi, Parking, SPA, Cafeteria"},
+                new Hotel{HotelName = "Centennial Hotel Tallinn", City = "Tallinn", Address = "15 Endla", Services = "Wi-fi, SPA"},
+                new Hotel{HotelName = "Dorpat Conference Hotel", City = "Tartu", Address = "Soola 6", Services = "Wi-fi, TV, SPA"},
+                new Hotel{HotelName = "V Spa & Conference Hotel", City = "Tartu", Address = "Riia 2", Services = "Wi-fi, Parking, SPA,Restaurant, Bar"},
+                new Hotel{HotelName = "Hedon Spa & Hotel", City = "Pärnu", Address = " Ranna Puiestee 1", Services = "Wi-fi, Parking, SPA,TV"},
+                new Hotel{HotelName = "Strand Spa & Conference Hotel", City = "Pärnu", Address = "Tammsaare pst 35", Services = "Wi-fi, Parking, SPA, Bar"},
+                new Hotel{HotelName = "Toila Spa Hotel ", City = "Toila", Address = "Ranna 12", Services = "Wi-fi, Parking, SPA, TV, Bar"},
+            };
+
+            var selectedHotel = from hotel in hotels
+                                where hotel.City == "Tallinn"                               
+                                select hotel;
+
+            int j = 0;
+
+            foreach(Hotel hotel in selectedHotel)
+            {
+                j++;
+                Console.WriteLine(j + ". " + hotel);
+            }
+
+
+            Console.Write("\n\nPress any key...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassAuthorApp/ClassAuthorApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace ClassAuthorApp
+{
+    class Program
+    {
+        class Author
+        {
+            public string LastName { get; set; }
+            public string FirstName { get; set; }
+
+            public override string ToString()
+            {
+                return "Author: " + LastName + " " + FirstName;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Array authors
+            Author[] authors =
+            {
+                new Author{LastName = "Morrison", FirstName = "Alex" },
+                new Author{LastName = "Blake", FirstName = "David"}
+            };
+
+            Console.WriteLine("\n\nVariant1. LOOP OPERATOR: FOR ");
+            for (int i = 0; i < authors.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + authors[i]);
+            }
+
+            Console.WriteLine(string.Concat(Enumerable.Repeat("=",20)));
+            //----------------------------------------------------------------
+            Console.WriteLine("\n\nVariant2. LOOP OPERATOR: FOREACH ");
+
+            int n = 0;
+
+            foreach(Author author in authors)
+            {
+                n++;
+                Console.WriteLine(n + ". "+ author);
+            }
+
+            //===============================================LINQ
+
+            Console.WriteLine("\nLinQ");
+
+            var selectedAuthor = from author in authors
+                                 where author.FirstName=="Alex"
+                                 orderby author.LastName
+                                 select author; // Весь массив
+
+            n = 0;
+
+            foreach(Author author in selectedAuthor)
+            {
+                n++;
+                Console.WriteLine(n + ". " + author);
+            }
+
+
+
+
+            Console.Write("\n\nPress any key");
+            Console.ReadKey();
+        }
+    }
+}
