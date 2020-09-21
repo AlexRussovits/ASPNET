1aec17b6f9730a1669010069ed02ebdbe3c75fa0
pupil <pupil@ivkhk.ee>
2020-09-21 15:10:10 +0300

    Start New Project

---
+++ ASPNET/MyPainterJPTVR18/MyPainterJPTVR18/Models/Painter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyPainterJPTVR18.Models
+{
+    public class Painter
+    {
+        public int Id { get; set; }
+        [Display(Name = "Фамилия и имя художника")]
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Biography { get; set; }
+        [Display(Name = "Фото")]
+        public byte[] Photo { get; set; }
+        public string PhotoType { get; set; }
+        public int? PictureId { get; set; }
+        public Picture Picture { get; set; }
+    }
+}
---
+++ ASPNET/MyPainterJPTVR18/MyPainterJPTVR18/Models/Picture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyPainterJPTVR18.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        [Display(Name = "Название картины")]
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Museum { get; set; }
+        [Display(Name = "Картина")]
+        public byte[] Canvas { get; set; }
+        public string CanvasType { get; set; }
+
+    }
+}
