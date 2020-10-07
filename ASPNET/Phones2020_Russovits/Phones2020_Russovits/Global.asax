0fee4c02584d878be8aec70d69b6ee740532f20a
pupil <pupil@ivkhk.ee>
2020-10-07 11:14:15 +0300

    Phones

---
+++ ASPNET/Phones2020_Russovits/Phones2020_Russovits/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Phones2020_Russovits.MvcApplication" Language="C#" %>
---
+++ ASPNET/Phones2020_Russovits/Phones2020_Russovits/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Phones2020_Russovits.Startup))]
+namespace Phones2020_Russovits
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
